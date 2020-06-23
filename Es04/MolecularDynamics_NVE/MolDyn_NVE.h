a7300697fbe120b5616664d2f9645a8644be8cfe
matteocamponovo <67024581+matteocamponovo@users.noreply.github.com>
2020-06-24 00:38:25 +0200

    Add files via upload

---
+++ Es04/MolecularDynamics_NVE/MolDyn_NVE.h
@@ -0,0 +1,53 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+//parameters, observables
+const int m_props=4;
+int n_props;
+int iv,ik,it,ie;
+double stima_pot, stima_kin, stima_etot, stima_temp;
+
+// averages
+double ave_pot, ave_kin, ave_temp, ave_etot;
+double media_pot, media_pot2, media_kin, media_kin2;
+double media_temp, media_temp2, media_etot, media_etot2;
+int blk_norm;
+
+//configuration
+const int m_part=108;
+double x[m_part],y[m_part],z[m_part],xold[m_part],yold[m_part],zold[m_part];
+double vx[m_part],vy[m_part],vz[m_part];
+
+// thermodynamical state
+int npart;
+double energy,temp,vol,rho,box,rcut,fs,eKb;
+
+// simulation
+int nstep, nblocks, iprint, seed,L,restart;
+double delta, block;
+
+//functions
+void Input(void);
+void Move(void);
+void ConfFinal(void);
+void Accumulate(void);
+void ConfXYZ(int);
+void Measure(void);
+void Blocco(double);
+double Force(int, int);
+double Pbc(double);
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
